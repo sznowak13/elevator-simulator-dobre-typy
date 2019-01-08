5d0da035b068991527e517206ba22659f9dbf0a7
Piotr <gonska9933@gmail.com>
2019-01-08 19:56:24 +0100

    Implement floor class

--- src/main/java/com/codecool/elevator/model/Floor.java
+++ src/main/java/com/codecool/elevator/model/Floor.java
@@ -2,18 +2,35 @@ package com.codecool.elevator.model;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Queue;
-
 
 public class Floor {
-    private HashMap<Direction, Queue<Person>> peopleQueue = new HashMap<Direction, Queue<Person>>();
+    private static int id;
+    private int level;
+    private HashMap<Direction, LinkedList<Person>> peopleQueue = new HashMap<Direction, LinkedList<Person>>();
     {
         peopleQueue.put(Direction.UP, new LinkedList<Person>());
         peopleQueue.put(Direction.DOWN, new LinkedList<Person>());
     };
 
+    Floor() {
+        level = id;
+        id++;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
     public void addPerson(Person person) {
-        //peopleQueue.get(person.getDesiredDirection()).add(person);
+        peopleQueue.get(person.getDesiredDirection()).add(person);
     };
 
+    public void removePersonFromQueue(Person person) {
+        Direction personDirection = person.getDesiredDirection();
+        peopleQueue.get(personDirection).remove(person);
+    }
+
+    public LinkedList<Person> getPeopleByDesiredDirection(Direction direction) {
+        return peopleQueue.get(direction);
+    }
 }
