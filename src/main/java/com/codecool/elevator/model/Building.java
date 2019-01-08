42bfabd521f5c513eaa8b7182ad93f5b24b39c24
Szymon Nowak <sznowak13@gmail.com>
2019-01-08 21:02:34 +0100

    Implemented constructors and array initialisers in Building

--- src/main/java/com/codecool/elevator/model/Building.java
+++ src/main/java/com/codecool/elevator/model/Building.java
@@ -6,15 +6,35 @@ public class Building {
     private Person[] peopleList;
 
     public Building() {
-
+        this(Consts.FLOORS_AMOUNT, Consts.ELEVATORS_AMOUNT);
     }
 
     public Building(int floorsAmount, int elevatorsAmount) {
+        createFloors(floorsAmount);
+        createElevators(elevatorsAmount);
+        createPeople(floorsAmount * Consts.MAX_FLOOR_CAP);
 
     }
 
-    public void createPeople() {
+    private void createPeople(int amount) {
+        this.peopleList = new Person[amount];
+        for (int i = 0; i < amount; i++) {
+            peopleList[i] = new Person();
+        }
+    }
 
+    private void createElevators(int amount) {
+        this.elevatorList = new Elevator[amount];
+        for (int i = 0; i < amount; i++) {
+            this.elevatorList[i] = new Elevator();
+        }
+    }
+
+    private void createFloors(int amount) {
+        this.floorList = new Floor[amount];
+        for (int i = 0; i < amount; i++) {
+            this.floorList[i] = new Floor();
+        }
     }
 
     public void addPersonToFloor(Person person, Floor floor) {
