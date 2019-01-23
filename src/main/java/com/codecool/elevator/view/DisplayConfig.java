0d6c9396469c903e67588946c7ffdf1a50b3a105
Piotr <gonska9933@gmail.com>
2019-01-23 15:35:06 +0100

    Added DisplayConfig variables

--- src/main/java/com/codecool/elevator/view/DisplayConfig.java
+++ src/main/java/com/codecool/elevator/view/DisplayConfig.java
@@ -1,13 +1,27 @@
 package com.codecool.elevator.view;
 
+import com.codecool.elevator.model.Config;
+import javafx.scene.paint.Color;
 import java.awt.*;
 
+
 public class DisplayConfig {
-    public final static double FLOOR_HEIGHT = 0;
-    public final static double FLOOR_WIDTH = 0;
-    public final static double ELEVATOR_WIDTH = 0;
-    public final static double PERSON_WIDTH = 0;
-    public final static double PERSON_HEIGHT = 0;
-    public final static double SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-    public final static double SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    public static double FLOOR_HEIGHT;
+    public static double FLOOR_WIDTH;
+    public static double ELEVATOR_WIDTH;
+    public static double PERSON_WIDTH;
+    public static double PERSON_HEIGHT;
+    public final static Color FLOOR_COLOR = Color.GRAY;
+    public final static Color ELEVATOR_COLOR = Color.RED;
+    public final static Color PERSON_COLOR = Color.BLUE;
+    public final static double SCREEN_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth()*0.9;
+    public final static double SCREEN_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight()*0.9;
+
+    public static void setup() {
+        FLOOR_HEIGHT = SCREEN_HEIGHT / Config.FLOORS_AMOUNT;
+        ELEVATOR_WIDTH = FLOOR_HEIGHT * 0.6; // scaling value of elevator width
+        FLOOR_WIDTH = SCREEN_WIDTH - (ELEVATOR_WIDTH * Config.ELEVATORS_AMOUNT);
+        PERSON_HEIGHT = FLOOR_HEIGHT / 2;
+        PERSON_WIDTH = ELEVATOR_WIDTH / 2;
+    }
 }
