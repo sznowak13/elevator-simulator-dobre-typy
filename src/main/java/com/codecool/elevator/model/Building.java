9e6d5307022553d5fd316a753af5027619624aa6
Piotr <gonska9933@gmail.com>
2019-01-23 15:27:28 +0100

    Implemented all create methods in Building

--- src/main/java/com/codecool/elevator/model/Building.java
+++ src/main/java/com/codecool/elevator/model/Building.java
@@ -1,27 +1,43 @@
 package com.codecool.elevator.model;
 
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Building implements Runnable{
     private static Building ourInstance = new Building();
     private List<Floor> floorList;
     private Queue<Person> peoplePool;
+    private ElevatorManager Andrzej;
 
 
     private Building() {
+        this.createElevatorManager();
+        this.createFloors();
+        this.createPeople();
     }
 
     public static Building getInstance() {
         return ourInstance;
     }
 
-    public void createPeople(int amount) {
+    public void createPeople() {
+        this.peoplePool = new LinkedList<>();
+
+        for (int i = 0; i < Config.PEOPLE_AMOUNT; i++) {
+            this.peoplePool.add(new Person());
+        }
+    }
+
+    public void createFloors() {
+        this.floorList = new ArrayList<>();
+
+        for (int i = 0; i < Config.FLOORS_AMOUNT; i++) {
+            this.floorList.add(new Floor(i));
+        }
 
     }
 
-    public void createFloors(int amount) {
-
+    public void createElevatorManager() {
+        this.Andrzej = ElevatorManager.getInstance();
     }
 
     public void createElevators(int amount) {
