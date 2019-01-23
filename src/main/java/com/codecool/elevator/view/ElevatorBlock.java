166ab3dfc303fc62923f2c7d3ed65d0ec341668c
Piotr <gonska9933@gmail.com>
2019-01-23 15:36:45 +0100

    Implemented constructor ElevatorBlock

--- src/main/java/com/codecool/elevator/view/ElevatorBlock.java
+++ src/main/java/com/codecool/elevator/view/ElevatorBlock.java
@@ -1,8 +1,16 @@
 package com.codecool.elevator.view;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class ElevatorBlock extends Rectangle {
+
+    public ElevatorBlock() {
+        super(DisplayConfig.ELEVATOR_WIDTH, DisplayConfig.FLOOR_HEIGHT);
+        setFill(DisplayConfig.ELEVATOR_COLOR);
+        setStroke(Color.BLACK);
+    }
+
     public void update() {
     }
 }
